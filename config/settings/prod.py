0a556b07f89df90ada20889ce87540a170646f1d
thompson <thompsonmina7@gmail.com>
2021-12-17 16:21:35 +0100

    curtain call

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 import dj_database_url
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', "sheltered-atoll-07813.herokuapp.com"]
 DATABASES = {
@@ -15,13 +15,6 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
-
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = "ryanthornston@gmail.com"
-# EMAIL_HOST_PASSWORD = "pseudoalias"
 
 EMAIL_BACKEND = "anymail.backends.mailjet.EmailBackend"
 ANYMAIL = {
