4ded8f35c1fede407d434f74d55b9133ca46ad66
Thompson <Thompsonmina7@gmail.com>
2022-03-10 11:49:14 +0100

    minor edit

--- config/settings/local.py
+++ config/settings/local.py
@@ -17,13 +17,6 @@ DATABASES = {
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = "ryanthornston@gmail.com"
-# EMAIL_HOST_PASSWORD = "pseudoalias"
-
 EMAIL_BACKEND = "anymail.backends.mailjet.EmailBackend"
 ANYMAIL = {
     "MAILJET_API_KEY": os.getenv("MAILJET_API_KEY"),
